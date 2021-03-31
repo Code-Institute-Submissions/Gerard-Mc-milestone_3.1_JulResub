c36e5a9523cccc4fd6742215e0614aceae82a628
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-03-31 14:23:02 +0000

    Add register page and view

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
-    
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'os.environ.get("MONGO_DBNAME")'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
-
 
 
 @app.route('/')
@@ -20,8 +20,12 @@ def find_gpus():
     return render_template("index.html", gpus=gpus)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
