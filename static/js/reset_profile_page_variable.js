780a61c81e6114978492b509597e3027ca019be4
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-06-16 17:42:53 +0000

    Add script to handle user GPU selection @profile.html

---
+++ static/js/reset_profile_page_variable.js
@@ -0,0 +1,5 @@
+// Resets the display style for the add/edit GPU links in profile.html.
+// This prevents them from not being visible if the user navigates away from the
+// page without finishing the GPU search/selection proccess.
+sessionStorage.setItem('link-display-1', 'contents');
+sessionStorage.setItem('link-display-2', 'contents');
